70aab443b9f05edbc8ff2648c222975351eeb642
Carla Crandall <carlalcrandall@gmail.com>
2017-04-25 15:08:39 -0400

    Renamed List to TaskList

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -1,15 +1,11 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { TaskCard } from './';
 
-export default class List extends PureComponent {
-
-    shouldComponentUpdate() {
-        return false;
-    }
+export default class TaskList extends Component {
 
     renderItem({item}) {
-        return <TaskCard {...item} />
+        return <TaskCard {...item} DELETE_TASK={this.props.DELETE_TASK} TOGGLE_TASK={this.props.TOGGLE_TASK} />
     }
 
     render() {
