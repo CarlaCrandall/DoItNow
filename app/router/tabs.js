9f93d9c019829067a575f602d95066f557fd7b40
Carla Crandall <carlalcrandall@gmail.com>
2017-04-26 12:36:28 -0400

    Style tabs individually based on list

--- app/router/tabs.js
+++ app/router/tabs.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import { TabNavigator, TabView } from 'react-navigation';
+import { Text, View } from 'react-native';
 import { Later, Now, Someday } from '../screens';
 import { TabNavigatorStyles } from '../styles/navigators';
 import { colors } from '../styles/vars';
 
+const renderTab = (routeName, focused) => {
+	const
+		tabName = routeName.charAt(0).toLowerCase() + routeName.slice(1),
+		tabStyles = [
+			TabNavigatorStyles.tab,
+			TabNavigatorStyles[`tab--${tabName}`]
+		];
+		labelStyles = [
+			TabNavigatorStyles.label,
+			TabNavigatorStyles[`label--${tabName}`]
+		];
+
+	return (
+		<View style={tabStyles}>
+			<Text style={labelStyles}>{routeName.toUpperCase()}</Text>
+		</View>
+	);
+}
+
 const Tabs = TabNavigator({
-	Now: { screen: Now },
-	Later: { screen: Later },
-	Someday: { screen: Someday }
+	Now: {
+		screen: Now,
+		navigationOptions: {
+			tabBarLabel: ({route, focused}) => renderTab(route.routeName, focused)
+		}
+	},
+	Later: {
+		screen: Later,
+		navigationOptions: {
+			tabBarLabel: ({route, focused}) => renderTab(route.routeName, focused)
+		}
+	},
+	Someday: {
+		screen: Someday,
+		navigationOptions: {
+			tabBarLabel: ({route, focused}) => renderTab(route.routeName, focused)
+		}
+	}
 },{
 	animationEnabled: true,
 	swipeEnabled: false,
 	tabBarComponent: TabView.TabBarTop,
 	tabBarPosition: 'top',
 	tabBarOptions: {
-		activeTintColor: colors.darkGray,
-		inactiveTintColor: colors.mediumGray,
+		pressOpacity: 1,
 		indicatorStyle: TabNavigatorStyles.indicator,
-		labelStyle: TabNavigatorStyles.label,
 		style: TabNavigatorStyles.tabBar
 	}
 });
--- app/styles/navigators/tab-navigator.js
+++ app/styles/navigators/tab-navigator.js
@@ -6,11 +6,38 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.white
 	},
 	indicator: {
-		backgroundColor: colors.darkGray
+		opacity: 0
 	},
+
+	// Tab Styles
+	tab: {
+		marginVertical: margins.xsmall,
+		borderBottomWidth: 2,
+	},
+	'tab--now': {
+		borderBottomColor: colors.red
+	},
+	'tab--later': {
+		borderBottomColor: colors.orange
+	},
+	'tab--someday': {
+		borderBottomColor: colors.blue
+	},
+
+	// Label Styles
 	label: {
+		padding: margins.xsmall / 2,
 		fontSize: fontSizes.medium,
 		fontWeight: fontWeights.normal
+	},
+	'label--now': {
+		color: colors.red
+	},
+	'label--later': {
+		color: colors.orange
+	},
+	'label--someday': {
+		color: colors.blue
 	}
 });
 
