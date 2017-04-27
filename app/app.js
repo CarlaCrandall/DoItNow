649890440076ed48d3dc027303912a1c9ad7350a
Carla Crandall <carlalcrandall@gmail.com>
2017-04-27 15:49:36 -0400

    Add support for Android back button

--- app/app.js
+++ app/app.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { addNavigationHelpers } from 'react-navigation';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
+import { BackAndroid, View } from 'react-native';
 import * as actions from './actions';
 import { AppContainerStyles } from './styles/containers';
 import { AppNavigator } from './navigators';
 
 class App extends Component {
+    componentDidMount() {
+        BackAndroid.addEventListener('backPress', () => this.handleBackPress(this.props));
+    }
+
+    componentWillUnmount() {
+        BackAndroid.removeEventListener('backPress');
+    }
+
+    handleBackPress({dispatch, navigation}) {
+        // Close the app if no route to go back to
+        if (navigation.index === 0) {
+            return false;
+        }
+
+        dispatch(NavigationActions.back());
+        return true;
+    }
+
     handleNavigationStateChange({SWIPEOUT_TASK}) {
         SWIPEOUT_TASK(null);
     }
