12cef14dcc759b2490e9a7464efdc2188e3143e7
Carla Crandall <carlalcrandall@gmail.com>
2017-05-03 14:47:15 -0400

    Persist redux state

--- app/store/index.js
+++ app/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from '../reducers';
 
-export const store = createStore(reducers);
+const store = createStore(reducers, autoRehydrate());
+
+// Persist redux state with redux-persist
+persistStore(store, { storage: AsyncStorage });
 
 export default store;
