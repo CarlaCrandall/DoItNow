3935c025c9f93e1a02569c917df179e98cec78df
Carla Crandall <carlalcrandall@gmail.com>
2017-05-15 17:50:32 -0400

    Tweak animation settings

--- app/components/animated-save-view.js
+++ app/components/animated-save-view.js
@@ -13,22 +13,29 @@ export default class AnimatedSaveView extends Component {
 			overlayOpacity: new Animated.Value(0),
 			iconSize: new Animated.Value(0),
 			textOpacity: new Animated.Value(0),
-			textPosition: new Animated.Value(25)
+			textPosition: new Animated.Value(50)
 		};
 	}
 
 	componentWillReceiveProps(nextProps) {
 		if (this.props.saving !== nextProps.saving && nextProps.saving) {
 			const
-				textOpacityAnimation = Animated.timing(this.state.textOpacity, { toValue: 1, duration: 150 }),
-				textPositionAnimation = Animated.timing(this.state.textPosition, { toValue: 0, duration: 150, easing: Easing.bounce });
+				// Animate text properties in parallel
+				parallelAnimation = Animated.parallel([
+					Animated.timing(this.state.textOpacity, { toValue: 1, duration: 550 }),
+					Animated.timing(this.state.textPosition, { toValue: 0, duration: 550, easing: Easing.in(Easing.bounce) })
+				]),
+				// Stagger animation of icon and text
+				staggeredAnimation = Animated.stagger(150, [
+					Animated.timing(this.state.iconSize, { toValue: 72, duration: 550, easing: Easing.in(Easing.bounce) }),
+					parallelAnimation
+				]);
 
 			// Animate overlay opacity, icon size, and text position
 			Animated.sequence([
 				Animated.timing(this.state.overlayOpacity, { toValue: 1, duration: 150 }),
-				Animated.timing(this.state.iconSize, { toValue: 72, duration: 150, easing: Easing.bounce }),
-				Animated.delay(200),
-				Animated.parallel([textOpacityAnimation, textPositionAnimation]),
+				Animated.delay(50),
+				staggeredAnimation,
 				Animated.delay(1500)
 			])
 			// Navigate back after animation completes
